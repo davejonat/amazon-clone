c0914898e1da121b69bd7781f449c678ed809294
David Jonathan <davejona@yahoo.com>
2020-09-09 21:24:42 +0100

    Deploy
    Amazon Clone 1st Debug

---
+++ firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCYwHLU7X15BeCs2Uhsnk_rtW62SvQHNeE",
+    authDomain: "clone-7fb1a.firebaseapp.com",
+    databaseURL: "https://clone-7fb1a.firebaseio.com",
+    projectId: "clone-7fb1a",
+    storageBucket: "clone-7fb1a.appspot.com",
+    messagingSenderId: "929125695222",
+    appId: "1:929125695222:web:35a0953a93d66115f4b6d7",
+    measurementId: "G-8E06S80SJL"
+  };
